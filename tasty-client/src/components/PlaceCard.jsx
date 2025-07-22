b9d35f150fe7d3033953ca31f6428bc6cba04531
ElviaChoi <elviachoi924@gmail.com>
2025-07-22 15:00:24 +0900

    PlaceCard 컴포넌트 기본 구현 (close #1)

---
+++ tasty-client/src/components/PlaceCard.jsx
@@ -0,0 +1,16 @@
+const PlaceCard = ({ title, image }) => {
+  return (
+    <div className="w-48 rounded-md overflow-hidden shadow border">
+      <img
+        src={`http://localhost:300${image}`}
+        alt={title}
+        className="w-full h-32 object-cover"
+      />
+      <div className="bg-gray-100 text-center py-2 text-sm font-medium">
+        {title}
+      </div>
+    </div>
+  );
+};
+
+export default PlaceCard;
