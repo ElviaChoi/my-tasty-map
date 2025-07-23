5220b8a1d75625baf3b52c0201993c5802c7cfa5
ElviaChoi <elviachoi924@gmail.com>
2025-07-24 00:50:41 +0900

    ✨ feat: 로딩 상태 처리 기능 구현

--- tasty-client/src/components/PlaceList.jsx
+++ tasty-client/src/components/PlaceList.jsx
@@ -4,13 +4,31 @@ import PlaceCard from "./PlaceCard";
 
 const PlaceList = () => {
   const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    getPlaces().then((data) => {
-      console.log("받은 데이터:", data);
-      setPlaces(data.places);
-    });
+    setLoading(true);
+    getPlaces()
+      .then((data) => {
+        setPlaces(data.places);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError("데이터를 불러오는 데 실패했습니다.");
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return (
+      <p className="text-gray-800 text-center">데이터를 불러오는 중입니다...</p>
+    );
+  }
+
+  if (error) {
+    return <p className="text-red-400 text-center">{error}</p>;
+  }
 
   return (
     <div className="flex flex-wrap gap-4 justify-center">
