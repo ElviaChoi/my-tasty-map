09aac1b64e084f4d05abad005d641bc61ede17cd
ElviaChoi <elviachoi924@gmail.com>
2025-07-24 23:32:30 +0900

    📍 feat: 사용자 위치 기반 맛집 정렬 기능 구현 (2-3 미션 완료)

--- tasty-client/src/components/PlaceList.jsx
+++ tasty-client/src/components/PlaceList.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import { getPlaces } from "../api/index";
+import { sortPlacesByDistance } from "../utils/loc";
+import { useGeoLocation } from "../hooks/useGeoLocation";
 import PlaceCard from "./PlaceCard";
 
 const PlaceList = () => {
+  const { location, error: locationError } = useGeoLocation();
+
   const [places, setPlaces] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -13,9 +17,18 @@ const PlaceList = () => {
         setLoading(true);
         setError(null);
         const data = await getPlaces();
-        setPlaces(data.places);
+
+        if (location) {
+          const sorted = sortPlacesByDistance(
+            data.places,
+            location.lat,
+            location.lon
+          );
+          setPlaces(sorted);
+        } else {
+          setPlaces(data.places);
+        }
       } catch (err) {
-        console.error("에러 발생:", err);
         setError(err.message);
       } finally {
         setLoading(false);
@@ -23,19 +36,19 @@ const PlaceList = () => {
     };
 
     fetchPlaces();
-  }, []);
+  }, [location]);
 
-  if (error) {
+  if (error || locationError) {
     return (
       <p className="bg-red-200 text-red-900 p-4 text-center font-semibold">
-        {error}
+        {error || locationError}
       </p>
     );
   }
 
   if (loading) {
     return (
-      <p className="text-gray-800 text-center">맛집을 불러오는 중입니다...</p>
+      <p className="text-gray-50 text-center">맛집을 불러오는 중입니다...</p>
     );
   }
 
